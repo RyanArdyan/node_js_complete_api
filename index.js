bcbac33052d921fa8e0dc524c6b7b4c27cde0a40
RyanArdyan <ryanardyan437@gmail.com>
2025-01-12 11:26:38 +0700

    Inisialisasi Project dan mulai coding

---
+++ index.js
@@ -0,0 +1,6 @@
+// variable express berisi memanggil library express yang sudah diinstal
+const express = require("express");
+
+// variable app berisi membuat objek express, untuk mengatur server, mendefinisikan rute dan mengelola middleware
+const app = express();
+
